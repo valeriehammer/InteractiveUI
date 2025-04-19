615b5aad60237ee52a963331596718dfdd85e007
Valerie Hammer <vkellogg@wisc.edu>
2025-04-19 09:28:06 -0500

    Added 3 UI Views to complete UI

--- InteractiveUI/ContentView.swift
+++ InteractiveUI/ContentView.swift
@@ -12,7 +12,21 @@ struct ContentView: View {
         VStack{
             Text("What is your name?")
                 .font(.title)
+            TextField("Type your name here...", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                .multilineTextAlignment(.center)
+                .font(.title)
+                .border(Color.gray, width: 1)
+            Button("Submit Name") {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
+            .font(.title2)
+            .buttonStyle(.borderedProminent)
+            .tint(.purple)
+//style of button - note that buttons have styles so that it all doesn't have to be written out.
         }
+//end VStack
+        .padding()
+//VStack padding
     
     }
 //end body
