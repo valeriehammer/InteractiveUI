d75858290587aa76e280a919463c86bf0d9d92fc
Valerie Hammer <vkellogg@wisc.edu>
2025-04-19 09:09:48 -0500

    Initial Commit

---
+++ InteractiveUI/InteractiveUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveUIApp.swift
+//  InteractiveUI
+//
+//  Created by Valerie Hammer on 4/19/25.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
